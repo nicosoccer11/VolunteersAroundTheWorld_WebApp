31a17e7e8b4a4023d94e80e36778b2c2860222ec
TBK123 <lol383tarek@hotmail.com>
2023-09-22 12:59:33 -0500

    removed omiauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['793309384892-e95dg1c44vl3tt9mvdorr7drtfejoemm.apps.googleusercontent.com'], ENV['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']
-end
-  
