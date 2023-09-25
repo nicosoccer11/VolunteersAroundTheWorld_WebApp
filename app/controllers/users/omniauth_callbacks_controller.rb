05abeaf01e456a14f98c542a06d376fde8098c2a
TBK123 <lol383tarek@hotmail.com>
2023-09-25 14:35:08 -0500

    changed the google sign in layout and fixed it to redirect to the the user dashboard

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -49,7 +49,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
+    stored_location_for(resource_or_scope) || user_dashboard_path
   end
 
   private
