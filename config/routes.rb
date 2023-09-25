9f464803eb7e4658eaed3af47525885d206e4d81
TBK123 <lol383tarek@hotmail.com>
2023-09-25 13:11:09 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,24 +9,21 @@ Rails.application.routes.draw do
   # Admin dashboard route
   get 'admin_dashboard', to: 'users#admin_dashboard', as: 'admin_dashboard'
   get 'user_dashboard', to: 'users#user_dashboard', as: 'user_dashboard'
-  get 'user_checkin', to: 'users#user_checkin', as: 'user_checkin'
-  post 'user_checkin', to: 'users#checkin'
-  get 'user_dashboard', to: 'users#user_dashboard'
   get 'admin_checkin', to: 'users#admin_checkin', as: 'admin_checkin'
+  get 'user_checkin', to: 'users#user_checkin', as: 'user_checkin'
   post 'admin_checkin', to: 'users#admin_checkin'
+  post 'user_checkin', to: 'users#checkin'
   get '/users/home', to: 'users#home', as: 'users_home'
   get '/events/index', to: 'events#index', as: 'events_index'
   get '/events/new', to: 'events#new', as: 'events_new'
+
   # Users routes
   resources :users do
     collection do
       put :grant_admin
-      get 'checkin'
-      post 'checkin'
     end
   end
 
   # Events routes
   resources :events, only: [:index, :new, :create, :destroy]
 end
-
