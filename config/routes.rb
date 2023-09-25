9c89a70190a5cbb5836c7077b3a7a6be221ab5d2
Alex Keener <alexkeener318@gmail.com>
2023-09-25 17:44:56 +0000

    Connects add admins to database

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,13 @@ Rails.application.routes.draw do
   get 'admin_dashboard', to: 'users#admin_dashboard', as: 'admin_dashboard'
   get 'admin_checkin', to: 'users#admin_checkin', as: 'admin_checkin'
   post 'admin_checkin', to: 'users#admin_checkin'
+  post 'add_admin', to: 'users#add_admin', as: 'admin_add_admin'
   get '/users/home', to: 'users#home', as: 'users_home'
   get '/events/index', to: 'events#index', as: 'events_index'
   get '/events/new', to: 'events#new', as: 'events_new'
   # Users routes
   resources :users do
-    collection do
-      put :grant_admin  # Action to grant admin status.
-    end
+    post 'add_admin', on: :collection
   end
 
   # Events routes
