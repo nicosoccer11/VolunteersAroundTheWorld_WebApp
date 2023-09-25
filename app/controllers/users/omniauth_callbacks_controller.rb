d7462652a496845c2744af0a36c794f73efb2709
TBK123 <lol383tarek@hotmail.com>
2023-09-25 15:30:33 -0500

    fixed up syntax

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -50,6 +50,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || user_dashboard_path
+    
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    #before_action :ensure_admin, only: [:admin_dashboard, :grant_admin]
     def home
     end
     
@@ -60,11 +59,6 @@ class UsersController < ApplicationController
     
   
     private
-  
-    # def ensure_admin
-    #   unless current_user&.is_admin?
-    #     redirect_to root_path, alert: "You are not authorized to access this page."
-    #   end
-    # end
+
   end
   
