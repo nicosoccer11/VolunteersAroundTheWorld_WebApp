7477394c39fbe20263c693dceef2a2ba3a2ed97c
Alex Keener <alexkeener318@gmail.com>
2023-09-25 23:42:14 +0000

    Fixes more testing errors

--- spec/feature/admin_check_spec.rb
+++ spec/feature/admin_check_spec.rb
@@ -26,10 +26,41 @@ RSpec.feature 'Admin Check-In', type: :feature do
     select 'Test Event', from: 'Select Event'
 
     # Submit the form
-    click_button 'Check In'
+    click_button 'Check-In'
 
     # Add assertions to check for expected behavior
     expect(page).to have_content('User checked in successfully')  # Replace with your flash message
+
+  end
+
+  scenario 'admin checks in a non-existant user' do
+    user = User.create(
+      first_name: 'no',
+      last_name: 'Doe',
+      email: 'fake@example.com',
+      isAdmin: true,
+      password: 'password'
+    )
+
+    event = Event.create(
+      name: 'Test Event',
+      date: Date.today
+    )
+
+    # Visit the admin check-in page
+    visit admin_checkin_path
+
+    # Fill in the form with user details and select the event
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Doe'
+    fill_in 'Email', with: 'aiodaiod@example.com'
+    select 'Test Event', from: 'Select Event'
+
+    # Submit the form
+    click_button 'Check-In'
+
+    # Add assertions to check for expected behavior
+    expect(page).to have_content('No user found with the specified first name, last name, and email.')  # Replace with your flash message
 
   end
 end
