8cf00ebe8f2ad11fd9c60c1a9bc4531b6e65eb89
TBK123 <lol383tarek@hotmail.com>
2023-10-16 09:35:01 -0500

    fixed merge conflicts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,6 +6,26 @@ module Users
       if user.persisted?
         flash[:success] = I18n.t('devise.omniauth_callbacks.success', kind: 'Google')
         sign_in_and_redirect user, event: :authentication
+        tempUser = User.find_by(email: user.email)
+        session[:user_email] = auth.info.email
+        if tempUser
+          puts 'User already exists'
+          session[:user_email] = auth.info.email
+        else
+          session[:new_user] = true
+          session[:user_email] = auth.info.email
+          User.create!(
+            first_name: auth.info.first_name,
+            last_name: auth.info.last_name,
+            email: auth.info.email,
+            isAdmin: false,
+            provider: auth.provider,
+            uid: auth.uid,
+            avatar_url: auth.info.avatar_url,
+            password: 'password',
+            phone_number: "",
+          )
+        end
       else
         session[:google_data] = user_data_from_auth
         redirect_to profile_setup_path
